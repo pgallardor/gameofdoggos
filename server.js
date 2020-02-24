7ecee4e4499982e1963025cb422f0ebfa3544030
Pedro <pedrogallardorobinson@gmail.com>
2020-02-23 21:04:02 -0300

    fix trucho

---
+++ server.js
@@ -0,0 +1,5 @@
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(process.env.PORT || 8080, function(){
+    console.log('Game running...');
+});
